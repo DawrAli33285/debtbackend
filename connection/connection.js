b1df2636b8f8b8f508bfe858cbd7ddc9f69db33f
DawrAli33285 <lemightyeagle@gmail.com>
2026-04-14 03:08:30 +0500

    new

--- connection/connection.js
+++ connection/connection.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
-// const connect=mongoose.connect('mongodb+srv://nanhidunia33285_db_user:dawarali@cluster0.pc3gg4c.mongodb.net')
-const connect=mongoose.connect('mongodb://127.0.0.1/debt')
+const connect=mongoose.connect('mongodb+srv://nanhidunia33285_db_user:dawarali@cluster0.pc3gg4c.mongodb.net')
+// const connect=mongoose.connect('mongodb://127.0.0.1/debt')
 
 module.exports=connect;
 
